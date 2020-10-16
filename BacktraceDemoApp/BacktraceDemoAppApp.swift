82db0c03c18ed4ff41a601fd523a3f7f8d0a773a
Vincent Lussenburg <vincent@lussenburg.net>
2020-10-16 16:38:41 -0600

    Initial Commit

---
+++ BacktraceDemoApp/BacktraceDemoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BacktraceDemoAppApp.swift
+//  BacktraceDemoApp
+//
+//  Created by Vincent on 10/16/20.
+//
+
+import SwiftUI
+
+@main
+struct BacktraceDemoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BacktraceDemoApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BacktraceDemoApp
+//
+//  Created by Vincent on 10/16/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
